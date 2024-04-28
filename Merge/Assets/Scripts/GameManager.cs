3905d7f651e3a15dcc4e046d578d39afcda52ee4
thedat07 <49479547+thedat07@users.noreply.github.com>
2024-04-28 16:13:21 +0700

    update

--- Merge/Assets/Scripts/GameManager.cs
+++ Merge/Assets/Scripts/GameManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.iOS;
-using UnityEngine.Android;
 
 public class GameManager : Singleton<GameManager>
 {
